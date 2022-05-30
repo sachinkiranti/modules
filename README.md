6d89247622ee959bd4d62e1d8e05971a0ae69485
sachinkiranti <sachinkiranti@gmail.com>
2022-05-30 10:55:30 +0545

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Modules
+
+<p align="center"><a href="https://raisachin.com.np" target="_blank"><img src="./art/logo.png" width="400"></a></p>
+
+`sachinkiranti/modules`
+
+## Under construction
+
+- Not ready for production 
---
+++ src/ModuleServicePorvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SachinKiranti\Modules;
+
+use Illuminate\Support\ServiceProvider;
+
+class ModuleServicePorvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
